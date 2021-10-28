cfe870a1de80ab3c7d370a532fed04a02ef26967
emislan <93058936+emislan@users.noreply.github.com>
2021-10-27 21:41:37 -0400

    Create README.md

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #Emelia's
+# NOTES:
+# Install: https://flask.palletsprojects.com/en/2.0.x/installation/
+# Quickstart: https://flask.palletsprojects.com/en/2.0.x/quickstart/
+# To View: http://127.0.0.1:5000/
 #
-#
-#
+
+
 from flask import Flask
 
 app = Flask(__name__)
