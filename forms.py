bf25c1fe3dbcf4f43bbaa0b6dfa0a6756d72e805
Mark J Ryan <90434341+MJR-24@users.noreply.github.com>
2021-11-01 20:15:29 -0400

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class NewMouthpieceForm(FlaskForm):
+    mpName = StringField('Mouthpiece name', validators=[DataRequired()])
+    mpType = StringField('Mouthpiece type', validators=[DataRequired()])
+    mpPicture = StringField('Mouthpiece picture (URL or filename)')
+    mpPrice = StringField('Mouthpiece Price')
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class NewCaseForm(FlaskForm):
+    caseName = StringField('Phone Case name', validators=[DataRequired()])
+    caseType = StringField('Phone Case type', validators=[DataRequired()])
+    casePicture = StringField('Phone Case picture (URL or filename)')
+    casePrice = StringField('Phone Case Price')
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
