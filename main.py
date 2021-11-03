360a2b0c7bb88a09d7ec50008bc0064b804d566c
Zachary Holderle <93222103+Zrholderle@users.noreply.github.com>
2021-11-03 14:40:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@
 
 from flask import Flask, render_template,flash, redirect
 
+from forms import NewCaseForm, NewMouthpieceForm
+
+
 class Mouthpiece:
 	def __init__(self,id,name,type,price,pic):
 		self.id = id
@@ -18,18 +21,17 @@ class Mouthpiece:
 		self.pic = pic   
     
 class PhoneCase:
-	def __init__(self,id,name,facing,type,price,pic):
+	def __init__(self,id,name,type,price,pic):
 		self.id = id
 		self.name = name
-        self.facing = facing
 		self.type = type
 		self.price = price
 		self.pic = pic
 
 Mouthpiecelist = []
-p = Mouthpiece(0,"Frank Kaspar ", "14", "Bb Soprano",650,"fluffy.jpg")
-p2 = Mouthpiece(1,"Woodwind Company","C6" , "Bb Soprano", 120.00, "rover.jpg")
-p3 = Mouthpiece(2,"Block Meliphone","K9", "Eb Soprano",500,"cod.jpg")
+p = Mouthpiece(0,"Frank Kaspar ", "Bb Soprano",650,"fluffy.jpg")
+p2 = Mouthpiece(1,"Woodwind Company", "Bb Soprano", 120.00, "rover.jpg")
+p3 = Mouthpiece(2,"Block Meliphone", "Eb Soprano",500,"cod.jpg")
 
 PhoneCaseList = []
 p = PhoneCase(0,"Frank Kaspar ", "Bb Soprano",650,"fluffy.jpg")
@@ -41,24 +43,30 @@ app = Flask(__name__)
 def welcome():
     return "<p>Hello, World!</p><hr><p>Welcome to Our Store!</p><hr><p>Here are our products</p><hr>"
 
+# mouthpiece not defined -- issue
+
 @app.route("/listmouthpieces")
-def list():
+def mplist():
 	mouthpieces = mouthpiece.query.all()
 	return render_template("list.html",mouthpieces=mouthpieces)
-@app.route("/listcases")
-def list():
-	pets = PhoneCases.query.all()
-	return render_template("list.html",PhoneCases=PhoneCases)
+
 
 @app.route("/detail/<int:pickamouthpiece>")
 def detail(pickamouthpiece):
 	mouthpiece = Mouthpiece.query.get(pickamouthpiece)
 	return render_template("detail.html",mouthpiece=mouthpiece)
 
-@app.route("/detail/<int:pickacase>")
-def detail(pickacase):
-	case = case.query.get(pickacase)
-	return render_template("detail.html",case=case)
+# IT ISNT LIKING HAVING 2 PRODUCTS (2 of each of the following types of code)
+
+# @app.route("/listcases")
+# def caselist():
+# 	pets = PhoneCase.query.all()
+# 	return render_template("list.html",case=case)
+
+# @app.route("/detail/<int:pickacase>")
+# def detail(pickacase):
+# 	case = PhoneCase.query.get(pickacase)
+# 	return render_template("detail.html",case=case)
 
 if __name__ == "__main__":
 	app.run()
