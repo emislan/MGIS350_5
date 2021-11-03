0c9e314d826f400ec14a19002597a8cebeef2c83
Zrholderle <93222103+Zrholderle@users.noreply.github.com>
2021-11-03 06:27:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,34 @@
 #
 
 
-from flask import Flask
+from flask import Flask, render_template,flash, redirect
 
+class Mouthpiece:
+	def __init__(self,id,name,type,price,pic):
+		self.id = id
+		self.name = name
+		self.type = type
+		self.price = price
+		self.pic = pic   
+    
+class PhoneCase:
+	def __init__(self,id,name,facing,type,price,pic):
+		self.id = id
+		self.name = name
+        self.facing = facing
+		self.type = type
+		self.price = price
+		self.pic = pic
+
+Mouthpiecelist = []
+p = Mouthpiece(0,"Frank Kaspar ", "14", "Bb Soprano",650,"fluffy.jpg")
+p2 = Mouthpiece(1,"Woodwind Company","C6" , "Bb Soprano", 120.00, "rover.jpg")
+p3 = Mouthpiece(2,"Block Meliphone","K9", "Eb Soprano",500,"cod.jpg")
+
+PhoneCaseList = []
+p = PhoneCase(0,"Frank Kaspar ", "Bb Soprano",650,"fluffy.jpg")
+p2 = PhoneCase(1,"Woodwind Company" , "Bb Soprano", 120.00, "rover.jpg")
+p3 = PhoneCase(2,"Block Meliphone", "Eb Soprano",500,"cod.jpg")
 app = Flask(__name__)
 
 @app.route("/")
