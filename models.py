0b3759b7e7829b9920f9df6d903ba6e8db10345e
Zachary Holderle <93222103+Zrholderle@users.noreply.github.com>
2021-11-04 14:00:46 -0400

    Update models.py

--- models.py
+++ models.py
@@ -3,12 +3,10 @@ from main import db
 
 class Mouthpiece(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	mpname = db.Column(db.String(64),index=True)
-	mptype = db.Column(db.String(64),index=True)
-	mpprice = db.Column(db.Float,index=True)
-	mppic = db.Column(db.String(256),index=True)
+	mpName = db.Column(db.String(64),index=True)
+	mpType = db.Column(db.String(64),index=True)
+	mpPrice = db.Column(db.Float,index=True)
+	mpPic = db.Column(db.String(256),index=True)
 	
 	def __repr__(self):
-		return '<Pet named {}>'.format(self.mpname)
-
-
+		return '<Pet named {}>'.format(self.mpName)
